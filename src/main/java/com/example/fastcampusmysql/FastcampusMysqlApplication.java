61cd4559b0e732aa01a4cc1b2cfb26024103d867
koogk7 <koogk7@gmail.com>
2022-08-07 23:47:37 +0900

    Initial commit

---
+++ src/main/java/com/example/fastcampusmysql/FastcampusMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastcampusmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fastcampusmysql/FastcampusMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastcampusmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
